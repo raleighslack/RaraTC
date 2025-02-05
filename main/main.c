bfd3fe48938933c6965c98727989ca6e832f7f6a
Raleigh Slack <raleighslack@gmail.com>
2025-02-05 14:39:23 -0500

    Added timer period test for timecode

--- main/main.c
+++ main/main.c
@@ -9,6 +9,7 @@
 #include "esp_mac.h"
 #include "esp_log.h"
 #include "driver/rmt_tx.h"
+#include "esp_timer.h"
 #include "smpte_timecode.h"
 #include "esp_sntp.h"
 #include <string.h>
@@ -19,7 +20,23 @@
 
 static const char *TAG = "main";
 
-const float period_ms = (((float)1/LTC_FRAMERATE)/LTC_BITS_PER_FRAME)*1000;
+static void periodic_timer_callback(void* arg); //stolen from example
+
+const esp_timer_create_args_t periodic_timer_args = {
+        .callback = &periodic_timer_callback,
+        /* name is optional, but may help identify the timer when debugging */
+        .name = "periodic"
+};
+esp_timer_handle_t periodic_timer;
+/* The timer has been created but is not running yet */
+
+const float period_us = (((float)1/LTC_FRAMERATE)/LTC_BITS_PER_FRAME)*10000;
+
+static void periodic_timer_callback(void* arg)
+{
+    int64_t time_since_boot = esp_timer_get_time();
+    ESP_LOGI(TAG, "Periodic timer called, time since boot: %lld us", time_since_boot);
+}
 
 void print_binary(uint8_t* bits[10]) {
     for (int i = 0; i < 10; i++) {
@@ -37,6 +54,8 @@ void print_binary(uint8_t* bits[10]) {
 
 void app_main(void)
 {
+    esp_timer_init();
+    ESP_ERROR_CHECK(esp_timer_start_periodic(periodic_timer, period_us));
     
     ltc_frame testframe = {};
     create_frame_from_timecode(&testframe, 21, 16, 29, 05);
@@ -48,6 +67,6 @@ void app_main(void)
     uint8_t *test[10] = {};
     create_bits_from_frame(test, testframe);
 
-    ESP_LOGI(TAG, "%.6f", period_ms);
+    ESP_LOGI(TAG, "%.6f", period_us);
     print_binary(test);
 }
